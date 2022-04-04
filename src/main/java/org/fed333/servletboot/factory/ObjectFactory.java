e84cc512cf721d37843da5063194887c8459d107
Fed333 <kovalchuk.roman03@gmail.com>
2022-04-05 01:17:40 +0300

    added ObjectFactory interface, and its implementation

--- src/main/java/org/fed333/servletboot/factory/ObjectFactory.java
+++ src/main/java/org/fed333/servletboot/factory/ObjectFactory.java
@@ -1,56 +1,11 @@
 package org.fed333.servletboot.factory;
 
-import org.fed333.servletboot.annotation.Singleton;
-import org.fed333.servletboot.configurator.ObjectConfigurator;
-import org.fed333.servletboot.context.ApplicationContext;
-
-import javax.annotation.PostConstruct;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.CopyOnWriteArrayList;
-
 /**
- * ObjectFactory class, for creating plain JavaBeans objects within ApplicationContext
- *
+ * ObjectFactory interface, for creating plain JavaBeans objects within ApplicationContext
  * @author Roman Kovalchuk
- * @version 1.2
+ * @version 1.0
  * */
-public class ObjectFactory {
-
-    /**
-     * Context within the infrastructure works.
-     * @since 1.0
-     * @see ApplicationContext
-     * */
-    private final ApplicationContext context;
-
-    /**
-     * List with ObjectConfigurators to configure plain JavaBean objects, after its creation
-     * @since 1.0
-     * @see ObjectConfigurator
-     * */
-    private final List<ObjectConfigurator> configurators = new CopyOnWriteArrayList<>();
-
-    /**
-     * Temporary cache for creation singleton objects with bilateral dependencies.
-     * @since 1.0
-     * */
-    private final Map<Class<?>, Object> cache = new HashMap<>();
-
-    public ObjectFactory(ApplicationContext context) {
-        this.context = context;
-
-        for(Class<? extends ObjectConfigurator> clazz : context.getConfig().getSystemScanner().getSubTypesOf(ObjectConfigurator.class)){
-            try {
-                configurators.add(clazz.getDeclaredConstructor().newInstance());
-            } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+public interface ObjectFactory {
 
     /**
      * Creates object according to the given class,
@@ -60,75 +15,6 @@ public class ObjectFactory {
      * @return Created and configured plain JavaBean object within ApplicationContext
      * @since 1.0
      * */
-    public <T> T createObject(Class<T> clazz){
+    <T> T createObject(Class<T> clazz);
 
-        try {
-            T object;
-            if (clazz.isAnnotationPresent(Singleton.class)){
-                return getSingleton(clazz);
-            }
-            object = create(clazz);
-            configureObject(object);
-            invokeInit(object);
-
-            return object;
-
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Failed to create object with factory!", e);
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    private <T> T getSingleton(Class<T> clazz) throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
-        T object;
-        if (cache.containsKey(clazz)){
-            object = (T) cache.get(clazz);
-        } else {
-            object = create(clazz);
-            cache.put(clazz, object);
-            configureObject(object);
-            invokeInit(object);
-        }
-        return object;
-    }
-
-    private <T> T create(Class<T> clazz) throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
-        return clazz.getDeclaredConstructor().newInstance();
-    }
-
-    private <T> void invokeInit(T object) {
-        Class<?> implClass = object.getClass();
-        for (Method method : implClass.getDeclaredMethods()) {
-            if (method.isAnnotationPresent(PostConstruct.class)){
-                method.setAccessible(true);
-                try {
-                    method.invoke(object);
-                } catch (IllegalAccessException | InvocationTargetException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    private <T> void configureObject(T object) {
-        configurators.forEach(configurator->configurator.configure(object, context));
-    }
-
-    /**
-     * Clears cached by factory singleton objects.
-     * @since 1.1
-     * */
-    public void clearCache(){
-        cache.clear();
-    }
-
-    /**
-     * Removes object of given class from {@link #cache}.
-     * @param clazz class of object
-     * @return removed instance of clazz, or null if no one was found
-     * */
-    public Object popClass(Class<?> clazz){
-        return cache.remove(clazz);
-    }
 }
---
+++ src/main/java/org/fed333/servletboot/factory/impl/BaseObjectFactory.java
@@ -0,0 +1,135 @@
+package org.fed333.servletboot.factory.impl;
+
+import org.fed333.servletboot.annotation.Singleton;
+import org.fed333.servletboot.configurator.ObjectConfigurator;
+import org.fed333.servletboot.context.ApplicationContext;
+import org.fed333.servletboot.context.impl.BaseApplicationContext;
+import org.fed333.servletboot.factory.ObjectFactory;
+
+import javax.annotation.PostConstruct;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * ObjectFactory class, for creating plain JavaBeans objects within ApplicationContext
+ * @author Roman Kovalchuk
+ * @version 1.2
+ * */
+public class BaseObjectFactory implements ObjectFactory {
+
+    /**
+     * Context within the infrastructure works.
+     * @since 1.0
+     * @see BaseApplicationContext
+     * */
+    private final ApplicationContext context;
+
+    /**
+     * List with ObjectConfigurators to configure plain JavaBean objects, after its creation
+     * @since 1.0
+     * @see ObjectConfigurator
+     * */
+    private final List<ObjectConfigurator> configurators = new CopyOnWriteArrayList<>();
+
+    /**
+     * Temporary cache for creation singleton objects with bilateral dependencies.
+     * @since 1.0
+     * */
+    private final Map<Class<?>, Object> cache = new HashMap<>();
+
+    public BaseObjectFactory(ApplicationContext context) {
+        this.context = context;
+
+        for(Class<? extends ObjectConfigurator> clazz : context.getConfig().getSystemScanner().getSubTypesOf(ObjectConfigurator.class)){
+            try {
+                configurators.add(clazz.getDeclaredConstructor().newInstance());
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Creates object according to the given class,
+     * the class must have constructor without arguments or default one
+     * @param clazz Type of object being created
+     * @throws RuntimeException when object creating was failed
+     * @return Created and configured plain JavaBean object within ApplicationContext
+     * @since 1.0
+     * */
+    public <T> T createObject(Class<T> clazz){
+
+        try {
+            T object;
+            if (clazz.isAnnotationPresent(Singleton.class)){
+                return getSingleton(clazz);
+            }
+            object = create(clazz);
+            configureObject(object);
+            invokeInit(object);
+
+            return object;
+
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to create object with factory!", e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> T getSingleton(Class<T> clazz) throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        T object;
+        if (cache.containsKey(clazz)){
+            object = (T) cache.get(clazz);
+        } else {
+            object = create(clazz);
+            cache.put(clazz, object);
+            configureObject(object);
+            invokeInit(object);
+        }
+        return object;
+    }
+
+    private <T> T create(Class<T> clazz) throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        return clazz.getDeclaredConstructor().newInstance();
+    }
+
+    private <T> void invokeInit(T object) {
+        Class<?> implClass = object.getClass();
+        for (Method method : implClass.getDeclaredMethods()) {
+            if (method.isAnnotationPresent(PostConstruct.class)){
+                method.setAccessible(true);
+                try {
+                    method.invoke(object);
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private <T> void configureObject(T object) {
+        configurators.forEach(configurator->configurator.configure(object, context));
+    }
+
+    /**
+     * Clears cached by factory singleton objects.
+     * @since 1.1
+     * */
+    public void clearCache(){
+        cache.clear();
+    }
+
+    /**
+     * Removes object of given class from {@link #cache}.
+     * @param clazz class of object
+     * @return removed instance of clazz, or null if no one was found
+     * */
+    public Object popClass(Class<?> clazz){
+        return cache.remove(clazz);
+    }
+}
